404f45a420d4bef535351f2fdf45bbb60fc7e61b
JJ <Demosaurus@users.noreply.github.com>
2020-03-10 12:13:53 +0000

    Add files via upload

---
+++ LAB6A.py
@@ -0,0 +1,9 @@
+import hashlib  #HASHLIB IMPORT
+
+hashstring = 'ecsc' #STRING
+hash = 'c89aa2ffb9edcc6604005196b5f0e0e4' #HASH
+
+if hashstring != 'c89aa2ffb9edcc6604005196b5f0e0e4' : #IF HASHSTRING IS NOT EQUAL OR THE SAME AS THE HASH PROVIDED
+    #USE ecsc STRING
+    hashstring = hashstring
+    print(hashstring) #PRINT HASH's
